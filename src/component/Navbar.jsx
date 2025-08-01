39d8de448c4da23e36021e3eebcdfe1bb613d5d9
Amin <mohmadamn69@gmail.com>
2025-08-01 18:19:22 +0330

    add and replace themeToggle to navBar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -16,6 +16,33 @@ export const Navbar = () => {
     const [isScrolled, setIsScrolled] = useState(false);
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    useEffect(() => {
+        const storedTheme = localStorage.getItem("theme");
+
+        if (storedTheme === "dark") {
+            document.documentElement.classList.add("dark");
+            setIsDarkMode(true);
+        } else {
+            localStorage.setItem("theme", "light");
+            setIsDarkMode(false);
+        }
+    }, [])
+
+    const toggleTheme = () => {
+        if (isDarkMode) {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("theme", "light");
+            setIsDarkMode(false);
+        } else {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("theme", "dark");
+            setIsDarkMode(true);
+        }
+
+    }
+
     useEffect(() => {
         const handleScroll = () => {
             setIsScrolled(window.scrollY > 200);
@@ -45,6 +72,13 @@ export const Navbar = () => {
                             {item.name}
                         </a>
                     ))}
+                    <button onClick={toggleTheme} className="fixed max-md:hidden left-5 top-4 z-50 p-2 rounded-full transition-colors duration-300 focus:outline-hidden">
+                        {isDarkMode ? (
+                            <Sun className="h-5 w-5 text-yellow-300" />
+                        ) : (
+                            <Moon className="h-5 w-5 text-blue-900" />
+                        )}
+                    </button>
                 </div>
                 {/* end of desktop navbar */}
 
