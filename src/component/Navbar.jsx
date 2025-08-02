b33c513174515ffea147082e88090db8136bc26b
Amin <mohmadamn69@gmail.com>
2025-08-02 07:26:51 +0330

    fix error in navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { cn } from "@/lib/utils.js"
-import { X, Menu, Sun, Moon } from "lucide-react"
+import { X, Menu } from "lucide-react"
 
 
 const navItems = [
@@ -53,12 +53,16 @@ export const Navbar = () => {
                     {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
                 </button>
                 <div className={cn(
-                    "fixed inset-0 bg-background/5 backdrop-blur-md z-40 flex flex-col items-center justify-center transition-all duration-300 md:hidden",
+                    "fixed inset-0 bg-background/95 backdrop-blur-md z-40 flex flex-col items-center justify-center transition-all duration-300 md:hidden",
                     isMenuOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none",
                 )}>
                     <div className="flex flex-col space-y-8 text-xl">
                         {navItems.map((item, key) => (
-                            <a key={key} href={item.href} className="text-foreground/80 hover:text-primary transition-colors duration-300" onClick={() => setIsMenuOpen(false)}>
+                            <a key={key}
+                                href={item.href}
+                                className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                                onClick={() => setIsMenuOpen(false)}
+                            >
                                 {item.name}
                             </a>
                         ))}
