4226ebf9e3eb06ed8fdcf643b30d3d869a27e86f
Amin <mohmadamn69@gmail.com>
2025-08-02 07:14:57 +0330

    fix error in navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -53,6 +53,7 @@ export const Navbar = () => {
                     {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
                 </button>
                 <div className={cn(
+                    "fixed inset-0 bg-background/5 backdrop-blur-md z-40 flex flex-col items-center justify-center transition-all duration-300 md:hidden",
                     isMenuOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none",
                 )}>
                     <div className="flex flex-col space-y-8 text-xl">
