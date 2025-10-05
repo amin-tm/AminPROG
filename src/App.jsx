8fb2cc1827338fd4c277a8b1d870c3a6fea59328
Amin <mohmadamn69@gmail.com>
2025-10-05 20:29:38 +0330

    fix error opening router

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home.jsx";
 import { NotFound } from "./pages/NotFound.jsx";
 import { Toaster } from "./component/ui/toaster.jsx"
-import AppType from "./pages/AppType.jsx"
+import AppType from "./pages/AppType"
 
 
 function App() {
@@ -12,7 +12,6 @@ function App() {
       <Routes>
         <Route index element={<Home />} />
         <Route path="/app-type" element={<AppType />} />
-        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   )
