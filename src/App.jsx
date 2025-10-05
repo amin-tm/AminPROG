88554c9c3770f7fe388ac8f824b71898086f8f83
Amin <mohmadamn69@gmail.com>
2025-10-05 20:33:46 +0330

    add vercel.json to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home.jsx";
-import { NotFound } from "./pages/NotFound.jsx";
 import { Toaster } from "./component/ui/toaster.jsx"
 import AppType from "./pages/AppType"
 
